446519e381c9808ad97e2d2ac140e1aa2f4d8271
ashi <ashi.berg@gmail.com>
2022-04-10 13:22:06 +0300

    created the class

---
+++ app.js
@@ -0,0 +1,8 @@
+class TrafficLight {
+    constructor(maxSpeed, time_till_next_change, traffic_light_state){
+        this.maxSpeed = maxSpeed
+        this.time_till_next_change = time_till_next_change
+        this.traffic_light_state = traffic_light_state
+        
+    }
+}
