09df8c65467733d2c98d6fc516ac5146fcda1f86
ashi <ashi.berg@gmail.com>
2022-04-10 16:21:08 +0300

    Finish the simulator

--- app.js
+++ app.js
@@ -5,48 +5,70 @@ class TrafficLight {
         this.lat = latitude;
         this.long = longitude;
 
-        this.timer = Math.floor(Math.random() * 10);
-        this.red_time = this.timer;
-        this.green_time = this.timer;
+        this.red_time = Math.floor(Math.random() * 10);
+        this.green_time = Math.floor(Math.random() * 10);
+        this.save_time = this.green_time;
         this.color = "green";
-        this.changeTrafficLight();
-        // setTimeout(() => { console.log("world"); }, 10000)
+        this.counter = 0 //number of cars that drive in red traffic light
+        this.total_counter = 0 //number of cars that drive in this traffic light section
+        console.log(this.counter)
+        this.carsDriveInRed();
+        this.carsDriveInThisTrafficLightSection();
+        setTimeout(() => { this.resetNumberOfCarsCounter(() => { this.counter = 0 }); }, 1000 * 60 * 10);
+        setTimeout(() => { () => { this.total_counter = 0 }; }, 1000 * 60 * 10);
+        //this.changeTrafficLight();
+        setTimeout(() => { this.changeTrafficLight(); }, 1000)
     }
     changeTrafficLight = () => {
         setInterval(() => {
-            if (this.color === "green"){
-                if (this.green_time > 0){
+            if (this.color === "green") {
+                if (this.green_time > 0) {
                     this.green_time -= 1;
                 }
-                else if(this.green_time == 0){
+                else if (this.green_time == 0) {
                     this.color = "red";
-                    this.green_time = this.timer;
+                    this.green_time = this.save_time;
+                    this.save_time = this.red_time;
                 }
             }
-            else{
-                if(this.red_time > 0){
+            else {
+                if (this.red_time > 0) {
                     this.red_time -= 1;
                 }
-                else if(this.red_time == 0){
+                else if (this.red_time == 0) {
                     this.color = "green";
-                    this.red_time = this.timer;
+                    this.red_time = this.save_time;
+                    this.save_time = this.green_time;
                 }
             }
         }
             , 1000);
     }
 
+    carsDriveInRed = () => {
+        setInterval(() => {
+            this.counter += Math.floor(Math.random() * 3);
+        }, 1000 * 60);
+    }
+
+    carsDriveInThisTrafficLightSection = () => {
+        setInterval(() => {
+            this.total_counter += Math.floor(Math.random() * 100);
+        }, 1000 * 30);
+    }
+
     getTrafficLightStatus = () => {
-        console.log('location', this.lat, this.long);
-        console.log('the traffic light will be ' + this.color + ' for the next ' + (this.color=="green" ? this.green_time : this.red_time))
+        console.log('Number of cars driving in red light in the past 10 minutes ' + this.counter);
+        console.log('Number of cars driving in this traffic light section ' + this.total_counter);
+        console.log('the traffic light will be ' + this.color + ' for the next ' + (this.color == "green" ? this.green_time : this.red_time))
     }
 }
 
-const d = new TrafficLight(maxSpeed = 60, time_till_next_change = 8, latitude = 56, longitude = 32)
+const firstTrafficLight = new TrafficLight(maxSpeed = 60, time_till_next_change = 8, latitude = 56, longitude = 32)
 
 change_traffic_light = () => {
     setInterval(() => {
-      d.getTrafficLightStatus()
+        firstTrafficLight.getTrafficLightStatus()
     }
         , 1000);
 }
